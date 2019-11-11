8914b53499bc1c9dc1dbda1977e5d0e1c6d873e5
JosephBarr-Uni <54667536+JosephBarr-Uni@users.noreply.github.com>
2019-11-11 18:13:44 +1000

    Added bomb class
    Added bomb class with some methods to initialise the game
    Begun the simonSays module

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # KeepTalkingSolver
  Keep Talking & Nobody Explodes Solver
+
+Plans:
+
+    Implement basic features for quick solving first
+
+    Add online web interface
+
+    Voice commands using the standard input from the first implementation of the program
+    
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 class SimonSays():
-    def __init__(self):
-        pass
+    def __init__(self, inBomb):
+        bomb = inBomb
 
     def __repr__(self):
         pass
 
+    # Dict mapping the colours to their output
+    map = {}
     def solve(self):
         colours = [input("Please Enter the First Colour: \n")]
 
@@ -12,4 +14,22 @@ class SimonSays():
 class Bomb():
     def __init__(self):
         strikes = 0
+        parallel = __setParallel()
+
+    def __setParallel(self):
+        def __getUserIn(): # Get the user input and convert it to upper case
+            userIn = input("Does the bomb have a parallel port? Y/N") # Get the user input
+            userIn = userIn.upper()
+            return userIn
+        
+        userIn = __getUserIn()
+        while(len(userIn) < 1 && (userIn[0] != "Y" || userIn[0] != "N")):
+            print("Please enter Y/N")
+            userIn = __getUserIn()
+        
+        if (userIn[0] == "Y"):
+            return True
+        else:
+            return False    
+
         
