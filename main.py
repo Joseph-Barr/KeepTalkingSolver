250c6532b8afeea977c0e0ce828444f755a73269
JosephBarr-Uni <n10469826@qut.edu.au>
2019-11-11 14:49:12 +1000

    Create Main.py
    Created a few basic classes and their methods

---
+++ main.py
@@ -0,0 +1,15 @@
+class SimonSays():
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def solve(self):
+        colours = [input("Please Enter the First Colour: \n")]
+
+
+class Bomb():
+    def __init__(self):
+        strikes = 0
+        
